7f5e0750e5000276b4fed3c9653f50e0cd57b297
yanniboii <538531@student.saxion.nl>
2023-09-05 12:17:14 +0200

    car

--- Assets/Scripts/MoveCar.cs
+++ Assets/Scripts/MoveCar.cs
@@ -5,31 +5,69 @@ using UnityEngine.UI;
 
 public class MoveCar : MonoBehaviour
 {
-    Rigidbody car;
+    public WheelCollider frontRight;
+    public WheelCollider frontLeft;
+    public WheelCollider backRight;
+    public WheelCollider backLeft;
+
+    public Transform frontRightTransform;
+    public Transform frontLeftTransform;
+    public Transform backRightTransform;
+    public Transform backLeftTransform;
+
+    public float acceleration = 500f;
+    public float breakForce = 300f;
+    public float turnAngle = 20f;
+
+    private float currentAcceleration = 0f;
+    private float currentBreakForce = 0f;
+    private float currentTurnAngle = 0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        car = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+        frontLeft.motorTorque= currentAcceleration;
+        frontRight.motorTorque = currentAcceleration;
+
+
+        currentTurnAngle = turnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        //UpdateWheel(frontLeft, frontLeftTransform);
+        //UpdateWheel(backLeft, backLeftTransform);
+        //UpdateWheel(backRight, backRightTransform);
+        //UpdateWheel(frontRight, frontRightTransform);
+    }
+
+    void UpdateWheel(WheelCollider col, Transform transform)
+    {
+        Vector3 pos;
+        Quaternion quaternion;
+        col.GetWorldPose(out pos, out quaternion);
+
+        transform.position = pos;
+        transform.rotation = quaternion;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            car.AddForce(new Vector3(0, 0, 2), ForceMode.Force);
-        }
+        
         if(Input.GetKey(KeyCode.S))
         {
-            car.AddForce(new Vector3(0,0,-2), ForceMode.Force);
         }
         if(Input.GetKey(KeyCode.D))
         {
-            car.MoveRotation(Quaternion);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            car.MoveRotation(new Quaternion(0, -1, 0, 0));
         }
     }
 }
