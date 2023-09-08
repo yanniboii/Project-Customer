af9d4c7d46d03a2833d928e5922611559f6065e7
yanniboii <yannekzessin@gmail.com>
2023-09-08 13:08:44 +0200

    boid avoid is killing me

--- Assets/Scripts/Boids.cs
+++ Assets/Scripts/Boids.cs
@@ -13,20 +13,28 @@ public class Boids : MonoBehaviour
 
     public GameObject boidPrefab;
 
-    public List<Boid> boids;
+    public List<Boid> boids = new List<Boid>();
 
-    private
+    private bool done;
     // Start is called before the first frame update
     void Start()
     {
         for(int i = 0; i < boidAmount; i++)
         {
-            Boid boid = new Boid(Instantiate(boidPrefab), boids, boidRange);
+            Boid boid = new Boid(Instantiate(boidPrefab), boidRange, boidSpeed);
             boid.boidPrefab.AddComponent<Rigidbody>();
-            boids.Add(boid);
-            if(i == boidAmount)
+            boid.boidPrefab.GetComponent<Rigidbody>().useGravity = false;
+            boids.Add(boid);   
+            if(i == boidAmount-1)
             {
-
+                done = true;
+            }
+        }
+        if(done)
+        {
+            for(int i = 0; i < boids.Count; i++ )
+            {
+                boids[i].UpdateBoids(boids);
             }
         }
     }
@@ -38,7 +46,14 @@ public class Boids : MonoBehaviour
         {
             for (int i = 0; i < boids.Count; i++)
             {
-                boids[i].boidPrefab.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1));
+                //boids[i].MoveForward();
+                if (boids[i].boids != null)
+                {
+                    boids[i].GetNearbyBoids();
+                }
+                if (boids[i].friends!= null)
+                {
+                }
             }
         }
 
@@ -69,26 +84,35 @@ public class Boids : MonoBehaviour
     }
 }
 
-public class Boid
+public class Boid : MonoBehaviour
 {
     public GameObject boidPrefab;
 
     public List<Boid> friends;
 
-    private List<Boid> boids;
+    public List<Boid> boids;
+
+    public float boidSpeed;
 
     private float boidRange;
 
-    public Boid(GameObject boidPrefab, List<Boid> boids, float boidRange)
+    public Boid(GameObject boidPrefab, float boidRange, float boidSpeed)
     {
         this.boidPrefab = boidPrefab;
-        this.boids = boids;
         this.boidRange = boidRange;
 
         friends = new List<Boid>();
+        this.boidSpeed = boidSpeed;
     }
 
-    void GetNearbyBoids()
+    private void Update()
+    {
+        Vector3 AvoidDir = CalculateSeparationDirection();
+        boidPrefab.GetComponent<Rigidbody>().velocity = AvoidDir.normalized * boidSpeed;
+    }
+
+
+    public void GetNearbyBoids()
     {
         List<Boid> nearby = new List<Boid>();
 
@@ -104,7 +128,44 @@ public class Boid
                 nearby.Add(test);
             }
         }
-
+        friends = nearby;
     }
+
+    public void MoveForward()
+    {
+        boidPrefab.GetComponent<Rigidbody>().AddForce(new Vector3(1,0,0));
+    }
+    public void UpdateBoids(List<Boid> boids)
+    {
+        this.boids = boids;
+    }
+
+   
+
+    public Vector3 CalculateSeparationDirection()
+    {
+        Vector3 separation = Vector3.zero;
+
+        foreach (Boid friend in friends)
+        {
+            if (friend != transform)
+            {
+                Vector3 offset = friend.boidPrefab.transform.position - boidPrefab.transform.position;
+                float distance = offset.magnitude;
+
+                // Check if the friend is too close
+                if (distance < boidRange)
+                {
+                    // Calculate a separation force based on the distance
+                    float separationFactor = 1.0f - (distance / boidRange);
+                    separation += -offset.normalized * separationFactor;
+                }
+            }
+        }
+
+        return separation;
+    }
+
+
 
 }
