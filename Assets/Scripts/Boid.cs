634bb6dd86f16f9582ea00ab85566ef7fbfcbfe9
yanniboii <yannekzessin@gmail.com>
2023-09-09 04:08:13 +0200

    boids work i think

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public GameObject boidPrefab;
+
+    public List<Transform> friends;
+
+    public float boidSpeed;
+
+    public float boidRange;
+
+    public float separationWeight;
+
+    public float alignmentWheight;
+
+    public float cohesionWeight;
+
+    public Boid(GameObject boidPrefab, float boidRange, float boidSpeed)
+    {
+        this.boidPrefab = boidPrefab;
+        this.boidRange = boidRange;
+
+        friends = new List<Transform>();
+        this.boidSpeed = boidSpeed;
+    }
+
+    private void Update()
+    {
+        DetectFriends();
+        Vector3 flockingDirection = CalcutlateFlockingDirection();
+        boidPrefab.GetComponent<Rigidbody>().velocity = flockingDirection * boidSpeed;
+    }
+
+
+    private void DetectFriends()
+    {
+        friends.Clear(); // Clear the list before detecting new friends
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, boidRange);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider != null && collider.gameObject != gameObject)
+            {
+                // Check if the detected object has the Boid script attached
+                Boid friendBoid = collider.gameObject.GetComponent<Boid>();
+                if (friendBoid != null)
+                {
+                    friends.Add(friendBoid.transform);
+                }
+            }
+        }
+    }
+
+
+
+    public Vector3 CalculateSeparationDirection()
+    {
+        Vector3 separation = Vector3.zero;
+
+        foreach (Transform friend in friends)
+        {
+            if (friend != transform)
+            {
+                Vector3 offset = friend.transform.position - boidPrefab.transform.position;
+                float distance = offset.magnitude;
+
+                // Check if the friend is too close
+                if (distance < boidRange)
+                {
+                    // Calculate a separation force based on the distance
+                    float separationFactor = 1.0f - (distance / boidRange);
+                    separation += -offset.normalized * separationFactor;
+                }
+            }
+        }
+
+        return separation;
+    }
+
+    private Vector3 CalculateAlignmentDirection()
+    {
+        Vector3 alignment = Vector3.zero;
+
+        foreach (Transform friend in friends)
+        {
+            if (friend != transform)
+            {
+                Vector3 offset = friend.position - transform.position;
+                float distance = offset.magnitude;
+
+                // Check if the friend is within alignment range
+                if (distance < boidRange)
+                {
+                    // Add the friend's velocity to the alignment vector
+                    Boid friendBoid = friend.GetComponent<Boid>();
+                    if (friendBoid != null)
+                    {
+                        alignment += friendBoid.boidPrefab.GetComponent<Rigidbody>().velocity;
+                    }
+                }
+            }
+        }
+
+        return alignment;
+    }
+    private Vector3 CalculateCohesionDirection()
+    {
+        Vector3 centerOfMass = Vector3.zero;
+        int count = 0;
+
+        foreach (Transform friend in friends)
+        {
+            if (friend != transform)
+            {
+                Vector3 offset = friend.position - transform.position;
+                float distance = offset.magnitude;
+
+                // Check if the friend is within cohesion range
+                if (distance < boidRange)
+                {
+                    centerOfMass += friend.position;
+                    count++;
+                }
+            }
+        }
+
+        if (count > 0)
+        {
+            // Calculate the average position of nearby friends
+            centerOfMass /= count;
+
+            // Calculate the direction towards the center of mass
+            Vector3 cohesionDirection = centerOfMass - transform.position;
+            return cohesionDirection;
+        }
+
+        // If no nearby friends, return zero vector
+        return Vector3.zero;
+    }
+
+    Vector3 CalcutlateFlockingDirection()
+    {
+        Vector3 alignementDirection = CalculateAlignmentDirection();
+        Vector3 separationDirection = CalculateSeparationDirection();
+        Vector3 cohesionDirection = CalculateCohesionDirection();
+
+        alignementDirection.Normalize();
+        separationDirection.Normalize();
+        cohesionDirection.Normalize();
+
+        Vector3 flockingDirection = alignementDirection * alignmentWheight + separationDirection * separationWeight + cohesionDirection * cohesionWeight;
+
+        return flockingDirection.normalized;
+    }
+
+
+}
--- Assets/Scripts/Boids.cs
+++ Assets/Scripts/Boids.cs
@@ -7,165 +7,22 @@ public class Boids : MonoBehaviour
 
     public int boidAmount;
 
-    public float boidSpeed;
-
-    public float boidRange;
-
     public GameObject boidPrefab;
-
-    public List<Boid> boids = new List<Boid>();
 
     private bool done;
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < boidAmount; i++)
+        for (int i = 0; i < boidAmount; i++)
         {
-            Boid boid = new Boid(Instantiate(boidPrefab), boidRange, boidSpeed);
-            boid.boidPrefab.AddComponent<Rigidbody>();
-            boid.boidPrefab.GetComponent<Rigidbody>().useGravity = false;
-            boids.Add(boid);   
-            if(i == boidAmount-1)
-            {
-                done = true;
-            }
-        }
-        if(done)
-        {
-            for(int i = 0; i < boids.Count; i++ )
-            {
-                boids[i].UpdateBoids(boids);
-            }
+            GameObject boid = Instantiate(boidPrefab);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(boids != null)
-        {
-            for (int i = 0; i < boids.Count; i++)
-            {
-                //boids[i].MoveForward();
-                if (boids[i].boids != null)
-                {
-                    boids[i].GetNearbyBoids();
-                }
-                if (boids[i].friends!= null)
-                {
-                }
-            }
-        }
 
     }
-
-
-
-    void Separtion(GameObject boid)
-    {
-        Physics.CheckSphere(boid.transform.position, boidRange);
-        for(int i = 0; i < boids.Count;i++)
-        {
-            if (Vector3.Distance(boid.transform.position, boids[i].boidPrefab.transform.position) < boidRange)
-            {
-
-            }
-        }
-    }
-
-    void Alignment()
-    {
-
-    }
-
-    void Cohesion()
-    {
-
-    }
-}
-
-public class Boid : MonoBehaviour
-{
-    public GameObject boidPrefab;
-
-    public List<Boid> friends;
-
-    public List<Boid> boids;
-
-    public float boidSpeed;
-
-    private float boidRange;
-
-    public Boid(GameObject boidPrefab, float boidRange, float boidSpeed)
-    {
-        this.boidPrefab = boidPrefab;
-        this.boidRange = boidRange;
-
-        friends = new List<Boid>();
-        this.boidSpeed = boidSpeed;
-    }
-
-    private void Update()
-    {
-        Vector3 AvoidDir = CalculateSeparationDirection();
-        boidPrefab.GetComponent<Rigidbody>().velocity = AvoidDir.normalized * boidSpeed;
-    }
-
-
-    public void GetNearbyBoids()
-    {
-        List<Boid> nearby = new List<Boid>();
-
-        for (int i = 0; i < boids.Count; i++)
-        {
-            Boid test = boids[i];
-            if (test == this)
-            {
-                return;
-            }
-            if (Vector3.Distance(test.boidPrefab.transform.position, boids[i].boidPrefab.transform.position) < boidRange)
-            {
-                nearby.Add(test);
-            }
-        }
-        friends = nearby;
-    }
-
-    public void MoveForward()
-    {
-        boidPrefab.GetComponent<Rigidbody>().AddForce(new Vector3(1,0,0));
-    }
-    public void UpdateBoids(List<Boid> boids)
-    {
-        this.boids = boids;
-    }
-
-   
-
-    public Vector3 CalculateSeparationDirection()
-    {
-        Vector3 separation = Vector3.zero;
-
-        foreach (Boid friend in friends)
-        {
-            if (friend != transform)
-            {
-                Vector3 offset = friend.boidPrefab.transform.position - boidPrefab.transform.position;
-                float distance = offset.magnitude;
-
-                // Check if the friend is too close
-                if (distance < boidRange)
-                {
-                    // Calculate a separation force based on the distance
-                    float separationFactor = 1.0f - (distance / boidRange);
-                    separation += -offset.normalized * separationFactor;
-                }
-            }
-        }
-
-        return separation;
-    }
-
-
 
 }
