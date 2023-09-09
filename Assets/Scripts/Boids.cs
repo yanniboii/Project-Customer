4807fcd3c594bd1806ad2d6cd1300710032efb77
yanniboii <yannekzessin@gmail.com>
2023-09-09 17:00:02 +0200

    i made a stupid box

--- Assets/Scripts/Boids.cs
+++ Assets/Scripts/Boids.cs
@@ -15,7 +15,8 @@ public class Boids : MonoBehaviour
     {
         for (int i = 0; i < boidAmount; i++)
         {
-            GameObject boid = Instantiate(boidPrefab);
+            Vector3 pos = new Vector3(Random.Range(0, 0.5f), Random.Range(0, 0.5f), Random.Range(0, 0.5f));
+            GameObject boid = Instantiate(boidPrefab, pos, Quaternion.identity);
         }
     }
 
