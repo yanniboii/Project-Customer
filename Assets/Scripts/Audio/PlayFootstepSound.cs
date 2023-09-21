a69891ca9abba3e3a707c6c6c1c48e0e6dc37421
Sooperfish <meinevis@gmail.com>
2023-09-22 00:39:50 +0200

    footie sounds :+1:

--- Assets/Scripts/Audio/PlayFootstepSound.cs
+++ Assets/Scripts/Audio/PlayFootstepSound.cs
@@ -7,6 +7,9 @@ public class PlayFootstepSound : MonoBehaviour
     private AudioSource audioSource;
     private bool isMoving;
     private bool isJumping;
+    public AudioClip[] sounds;
+    [Range(0.1f,0.5f)]
+    public float pitchChangeMultiplier = 0.2f;
 
     void Start()
     {
@@ -15,6 +18,10 @@ public class PlayFootstepSound : MonoBehaviour
 
     void Update()
     {
+        audioSource.clip = sounds[Random.Range(0, sounds.Length)];
+        audioSource.pitch = Random.Range(1 - pitchChangeMultiplier, 1 + pitchChangeMultiplier);
+                
+        
         if (GetComponent<Rigidbody>().velocity.magnitude > 0) isMoving = true;
         else isMoving = false;
 
@@ -22,7 +29,7 @@ public class PlayFootstepSound : MonoBehaviour
         else isJumping = true;
 
 
-        if (isMoving && !audioSource.isPlaying && !isJumping) audioSource.Play(); // if player is moving, not jumping and audiosource is not playing play it
+        if (isMoving && !audioSource.isPlaying && !isJumping) audioSource.PlayOneShot(audioSource.clip); // if player is moving, not jumping and audiosource is not playing play it
         if (!isMoving || isJumping) audioSource.Stop(); // if player is not moving or is jumping and audiosource is playing stop it
     }
 }
