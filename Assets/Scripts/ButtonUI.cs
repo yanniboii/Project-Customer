0e70c6e78931dc6c43279cdbec81bb98479f8376
yanniboii <538531@student.saxion.nl>
2023-09-21 13:20:43 +0200

    Unlock Done

--- Assets/Scripts/ButtonUI.cs
+++ Assets/Scripts/ButtonUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonUI : MonoBehaviour
 {
@@ -15,74 +16,208 @@ public class ButtonUI : MonoBehaviour
 
     public void UnlockLavender()
     {
-        if (moneyUI.money >= 25)
+        if (moneyUI.money >= 25 && !throwableSeeds.lavenderUnlocked)
         {
             throwableSeeds.lavenderUnlocked = true;
             moneyUI.money -= 25;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockMoss()
     {
-        if (moneyUI.money >= 60)
+        if (moneyUI.money >= 60 && !throwableSeeds.mossUnlocked)
         {
             throwableSeeds.mossUnlocked = true;
             moneyUI.money -= 60;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockMushroom()
     {
-        if (moneyUI.money >= 130)
+        if (moneyUI.money >= 130 && !throwableSeeds.mushUnlocked)
         {
             throwableSeeds.mushUnlocked = true;
             moneyUI.money -= 130;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockHolly()
     {
-        if (moneyUI.money >= 45)
+        if (moneyUI.money >= 45 && !throwableSeeds.hollyUnlocked)
         {
             throwableSeeds.hollyUnlocked = true;
             moneyUI.money -= 45;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockBerry()
     {
-        if (moneyUI.money >= 80)
+        if (moneyUI.money >= 80 && !throwableSeeds.berryUnlocked)
         {
             throwableSeeds.berryUnlocked = true;
             moneyUI.money -= 80;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockFern()
     {
-        if (moneyUI.money >= 200)
+        if (moneyUI.money >= 200 && !throwableSeeds.fernUnlocked)
         {
             throwableSeeds.fernUnlocked = true;
             moneyUI.money -= 200;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockHazel()
     {
-        if (moneyUI.money >= 60)
+        if (moneyUI.money >= 60 && !throwableSeeds.hazelUnlocked)
         {
             throwableSeeds.hazelUnlocked = true;
             moneyUI.money -= 60;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockOak()
     {
-        if (moneyUI.money >= 100)
+        if (moneyUI.money >= 100 && !throwableSeeds.oakUnlocked)
         {
             throwableSeeds.oakUnlocked = true;
             moneyUI.money -= 100;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void UnlockAsh()
     {
-        if (moneyUI.money >= 300)
+        if (moneyUI.money >= 300 && !throwableSeeds.ashUnlocked)
         {
             throwableSeeds.ashUnlocked = true;
             moneyUI.money -= 300;
+            Image childImage = null;
+            foreach (Transform child in transform)
+            {
+                childImage = child.GetComponent<Image>();
+                if (childImage != null)
+                {
+                    break;
+                }
+            }
+
+            // If an Image component is found, disable it
+            if (childImage != null)
+            {
+                childImage.enabled = false;
+            }
         }
     }
     public void Unlock()
